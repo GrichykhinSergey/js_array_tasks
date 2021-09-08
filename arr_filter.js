91ade5022a87b4c29551fd100e22f747224ee900
GrichykhinSergey <90179867+GrichykhinSergey@users.noreply.github.com>
2021-09-08 12:39:33 +0300

    Update arr_filter.js

--- arr_filter.js
+++ arr_filter.js
@@ -1,3 +1,3 @@
 let arr = [31, 10, 'chicken', 9, 'fish', 10];
-alert(arr.filter(item => isNaN(+item)));
+alert(arr.filter(item => typeof(item) === 'string'));
 alert(arr.filter(item => item == 10));
